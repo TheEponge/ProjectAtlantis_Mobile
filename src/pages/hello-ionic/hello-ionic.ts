d768c912d12bbb8a1aabe21f9be0c1904b950ebb
TheEponge <quentin.miceli@viacesi.fr>
2018-06-27 14:11:04 +0200

    ui change

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
 
+import { AlertController } from 'ionic-angular';
+
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  constructor(public alertCtrl: AlertController) {
+
+  }
+
+  loginPrompt(){
+
+    const prompt = this.alertCtrl.create({
+      title: 'Login',
+      inputs: [
+        {
+          name: 'Username',
+          placeholder: 'Username'
+        },
+        {
+          name: 'Password',
+          placeholder: 'Password',
+          type: 'password'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Sign in',
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+    prompt.present();
 
   }
 }
